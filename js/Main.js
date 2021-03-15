027c1094c942ff0f03a685ba5712d4fb45311973
shairanshrawat <62132905+shairanshrawat@users.noreply.github.com>
2021-03-15 22:48:39 +0530

    update

---
+++ js/Main.js
@@ -0,0 +1,19 @@
+//--------------------------------------FOOTER STARTS-------------------------------------//
+$(document).ready(function () {
+     // click to scroll top
+     $('.move-up span').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
+    })
+
+    // AOS Instance
+    AOS.init();
+   
+});    
+//-------------------------------------FOOTER ENDS---------------------------------------//
+
+
+
+
+  
